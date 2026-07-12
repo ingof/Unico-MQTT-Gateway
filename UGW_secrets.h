b0f77fa070da4f015b11b7fee026a110312badd5
Ingo F. <ingo.fischer@arcormail.de>
2026-07-12 19:54:33 +0200

    Create UGW_secrets.h

---
+++ UGW_secrets.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// WLAN Client
+const char* UGW_WIFI_SSID     = "DEIN_WLAN_NAME";
+const char* UGW_WIFI_PASSWORD = "DEIN_WLAN_PASSWORT";
+
+// Fallback Access Point, falls WLAN-Verbindung fehlschlaegt
+const char* UGW_AP_SSID       = "Unico-Gateway";
+const char* UGW_AP_PASSWORD   = "12345678";
+
+// MQTT Broker
+const char* UGW_MQTT_HOST     = "192.168.178.10";
+const uint16_t UGW_MQTT_PORT  = 1883;
+const char* UGW_MQTT_USER     = "";
+const char* UGW_MQTT_PASSWORD = "";
+const char* UGW_MQTT_CLIENTID = "unico-gateway-esp32";
+const char* UGW_MQTT_ROOT     = "unico";
