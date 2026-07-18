2478c66c4f30ed6268622ab8cb9a005a101e2daa
Ingo F. <software@fischer360.de>
2026-07-18 22:36:04 +0200

    Update UGW_secrets.h
    V1.4.5:
    added ntp configuration

--- UGW_secrets.h
+++ UGW_secrets.h
@@ -4,7 +4,7 @@
 const char* UGW_WIFI_SSID     = "DEIN_WLAN_NAME";
 const char* UGW_WIFI_PASSWORD = "DEIN_WLAN_PASSWORT";
 
-// Fallback Access Point, falls WLAN-Verbindung fehlschlaegt
+// Fallback Access Point, if WLAN-connection fails
 const char* UGW_AP_SSID       = "Unico-Gateway";
 const char* UGW_AP_PASSWORD   = "12345678";
 
@@ -15,3 +15,8 @@ const char* UGW_MQTT_USER     = "";
 const char* UGW_MQTT_PASSWORD = "";
 const char* UGW_MQTT_CLIENTID = "unico-gateway-esp32";
 const char* UGW_MQTT_ROOT     = "unico";
+
+// NTP-Zeitserver
+// possibel are one hostname or one IPv4-Adress.
+const char* UGW_NTP_SERVER  = "time.cloudflare.com";
+//const char* UGW_NTP_SERVER  = "192.168.1.1";
